179010f30976c3e8651290dc11838546a88cdf5b
shamsudeenabu <85015840+shamsudeenabu@users.noreply.github.com>
2021-05-29 12:51:30 +0100

    Add files via upload

---
+++ stanbic IBTC USSD code by shamsudeen.py
@@ -0,0 +1,46 @@
+
+print("welcome to stanbic bank IBTC")
+option=input("")
+print("1.Buy airtime\n2.Buy data\n3.@Ease wallet\n4Tranfer\n5.pay bills\n6.My balance\n7Open account\EZ cash\n8. Next")
+option=input("")
+if (option=="1"):
+    amount=input("Enter Amount")
+    pin=input("Enter pin")
+    print("great recharge successful")
+    back=input("Enter 1 To go back to menu")
+    print("Enter 2 to next")
+    option=input("")
+    if (option=="2"):
+    		amount=input("Enter Amount")
+    		number=input("Enter phone number")
+    		pin=input("Enter pin")
+    		print("recharge successful")
+    		option=input("")
+    		if (option=="3"):
+    			print("1.150mb=150N\n2.300=300N")
+    			if (option=="1"):
+    			 amount=input("Enter Amount")
+    			number=input("Enter phone number")
+    			print("Enter 1 to confrim")
+    			confirm=input("press 1")
+    			print("data recharge successful")
+    			option=input("")
+    			if (option=="4"):
+    				account_number=input("Enter the acct number")
+    			print(" the name of the account is ABUBAKAR SHAMSUDEEN")
+    			amount=input("Enter Amount")
+    			confirm_amount=input("Enter 1 to continue")
+    			pin=input("Enter four pin")
+    			print("transfer successful")
+    			print("thanks for banking with us")
+    			option=input("")
+    			if(option=="5"):
+    				account_number=input("Enter Account Number")
+    				print("1.polaries\n2.access\n3.GtB\n4.first bank\n5.jahiz bank\n6.Others")
+    				if(option=="1"):
+    					print("the account name is ABUBAKAR SHAMSUDEEN")
+    					
+    						
+    			
+    		
+    							
